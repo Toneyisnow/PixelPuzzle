21c61e748fd6e7ae2ce014cc45f27f65425c1e96
Toney Sui <charlie_5899@hotmail.com>
2019-07-02 14:08:56 -0700

    Completed StageGenerator.

---
+++ Tools/StageGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageGenerator
+{
+    /// <summary>
+    /// This command line tool is to help generate the data file for the poem stage.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generator generator = new Generator();
+            generator.Generate("sample_input.json", "sample_output.json");
+        }
+        
+
+    }
+}
